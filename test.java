3e861be8184a26773e2591ef63a2e46b83152602
gunanjr <bollugunawanth@gmail.com>
2025-03-08 20:21:45 +0530

    First commit

---
+++ test.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class test{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter the Number:");
+        // int a = sc.nextInt();
+
+        // int[] lolarr = new int[10];
+        // lolarr[0] = 2;
+        // lolarr[1] = 5;
+        System.out.println(6/2);
+
+
+        // System.out.println(a);
+    }
+}
